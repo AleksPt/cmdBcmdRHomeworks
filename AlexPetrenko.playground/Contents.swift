002ed835822e9e66418098775e53c5907040dd44
Alex Petrenko <alex.pt@ya.ru>
2024-01-17 13:55:59 +0300

    Task 2 complete

--- AlexPetrenko.playground/Contents.swift
+++ AlexPetrenko.playground/Contents.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-// MARK: - Task 1
+// MARK: - Task 1 "Зарплата мечты"
 let rateUsdToRub = 89
 var salaryRub = 300_000
 let salaryUsd = salaryRub / rateUsdToRub
@@ -11,3 +11,9 @@ print("""
 Зарплата в рублях = \(salaryRub)
 Зарплата в долларах = \(salaryUsd)
 """)
+
+// MARK: - Task 2 "Площадь треугольника"
+let triangleFoundation = 10
+let triangleHeight = 12
+let triangleSquare = triangleHeight * triangleFoundation / 2
+
